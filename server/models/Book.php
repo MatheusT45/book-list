11fe36159091c48b802287e8e47a1c38661fb867
Matheus Tavares <matheus.tavares45@gmail.com>
2024-01-12 17:59:19 -0300

    add book model and controller

---
+++ server/models/Book.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\behaviors\BlameableBehavior;
+use yii\behaviors\TimestampBehavior;
+
+/**
+ * This is the model class for table "books".
+ *
+ * @property int $id
+ * @property string|null $title
+ * @property string|null $description
+ * @property string|null $author
+ * @property int|null $total_pages
+ * @property string|null $created_at
+ */
+class Book extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'books';
+    }
+
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => TimestampBehavior::class,
+                'updatedAtAttribute' => false,
+            ],
+            [
+                'class' => BlameableBehavior::class,
+                'updatedByAttribute' => false,
+                'createdByAttribute' => false,
+            ]
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['description'], 'string'],
+            [['total_pages', 'created_at'], 'integer'],
+            [['title', 'author'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'description' => 'Description',
+            'author' => 'Author',
+            'total_pages' => 'Total Pages',
+            'created_at' => 'Created At',
+        ];
+    }
+}
