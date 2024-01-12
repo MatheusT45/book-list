79f64243923156c45075ac055eb04a866f045db6
Matheus Tavares <matheus.tavares45@gmail.com>
2024-01-12 19:04:35 -0300

    add authentication to book controller

--- server/controllers/BookController.php
+++ server/controllers/BookController.php
@@ -3,9 +3,21 @@
 namespace app\controllers;
 
 use app\models\Book;
+use yii\filters\auth\HttpBearerAuth;
 use yii\rest\ActiveController;
 
 class BookController extends ActiveController
 {
    public $modelClass = Book::class;
+
+   public function behaviors()
+   {
+      $behaviors = parent::behaviors();
+
+      $behaviors['authenticator'] = [
+         'class' => HttpBearerAuth::className(),
+      ];
+
+      return $behaviors;
+   }
 }
