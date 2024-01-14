354b377824b69884fc989df1d5acae376f83895c
Matheus Tavares <matheus.tavares45@gmail.com>
2024-01-14 12:56:47 -0300

    add books service and http client to angular app

--- web/src/app/books/books/books.component.ts
+++ web/src/app/books/books/books.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { BooksService } from '../services/books.service';
 import { Book } from '../models/book';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-books',
@@ -8,23 +10,10 @@ import { Book } from '../models/book';
   styleUrl: './books.component.scss',
 })
 export class BooksComponent {
+  books: Observable<Book[]>;
   displayedColumns: string[] = ['title', 'author', 'description', 'totalPages'];
-  books: Book[] = [
-    {
-      id: 1,
-      title: 'The Hobbit',
-      author: 'J.R.R. Tolkien',
-      description:
-        'The hobbit tells a fantastic story of adventure, danger, friendship, and courage, and serves as a prequel to the even more immersive and epic book series, the Lord of the Rings.',
-      totalPages: 310,
-    },
-    {
-      id: 2,
-      title: 'Percy Jackson and the Olympians',
-      author: 'Rick Riordan',
-      description:
-        'Percy Jackson is a good kid, but he cant seem to focus on his schoolwork or control his temper. And lately, being away at boarding school is only getting worse - Percy could have sworn his pre-algebra teacher turned into a monster and tried to kill him.',
-      totalPages: 375,
-    },
-  ];
+
+  constructor(private booksService: BooksService) {
+    this.books = this.booksService.list();
+  }
 }
