8d5799e739e7d9c9b7780e9403b5b753279c45e1
Matheus Tavares <matheus.tavares45@gmail.com>
2024-01-15 19:29:22 -0300

    add book filter to backend actions

--- server/controllers/BookController.php
+++ server/controllers/BookController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use app\models\Book;
 use yii\rest\ActiveController;
+use yii\base\DynamicModel;
 
 class BookController extends ActiveController
 {
@@ -20,5 +21,25 @@ class BookController extends ActiveController
       ];
 
       return $behaviors;
+   }
+
+   public function actions()
+   {
+      $actions = parent::actions();
+      
+      $actions['index']['dataFilter'] = [
+         'class' => \yii\data\ActiveDataFilter::class,
+         'attributeMap' => [
+            'totalPages' => 'total_pages',
+        ],
+         'searchModel' => (new DynamicModel(['id', 'title']))
+            ->addRule(['id'], 'integer')
+            ->addRule(['title'], 'string', ['min' => 2, 'max' => 200])
+            ->addRule(['description'], 'string', ['min' => 2, 'max' => 200])
+            ->addRule(['author'], 'string', ['min' => 2, 'max' => 200])
+            ->addRule(['totalPages'], 'integer'),
+      ];
+      
+      return $actions;
    }
 }
