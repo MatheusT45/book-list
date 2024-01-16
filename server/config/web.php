4f8bf3a1a1b3cfd2cdddbe1bb2d9b3fbc92216c5
Matheus Tavares <matheus.tavares45@gmail.com>
2024-01-15 23:37:15 -0300

    remove unused files from backend

--- server/config/web.php
+++ server/config/web.php
@@ -34,7 +34,6 @@ $config = [
         'mailer' => [
             'class' => \yii\symfonymailer\Mailer::class,
             'viewPath' => '@app/mail',
-            // send all mails to a file by default.
             'useFileTransport' => true,
         ],
         'log' => [
